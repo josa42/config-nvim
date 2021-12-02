33bac5dc9f34f655995e2315da6e3f644d6b8c44
Josa Gesell <josa@gesell.me>
2021-12-02 06:44:29 +0100

    chore: Use karb94/neoscroll.nvim for smooth scrolling

--- lua/jg/layers/scrolling.lua
+++ lua/jg/layers/scrolling.lua
@@ -1,5 +1,13 @@
 local layer = require('jg.lib.layer')
 
 layer.use({
+  enabled = false,
   require = { 'psliwka/vim-smoothie' },
+})
+
+layer.use({
+  require = { 'karb94/neoscroll.nvim' },
+  after = function()
+    require('neoscroll').setup()
+  end,
 })
