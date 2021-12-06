62a77de8c8bfe3f77180340ec348b340662c60b9
Josa Gesell <josa@gesell.me>
2021-12-06 20:04:48 +0100

    feat: Bind cycle history in telescope prompts

--- lua/jg/layers/fuzzy-finder--telescope.lua
+++ lua/jg/layers/fuzzy-finder--telescope.lua
@@ -95,6 +95,9 @@ layer.use({
         entry_prefix = '  ',
         default_mappings = {
           i = {
+            ['<C-Down>'] = actions.cycle_history_next,
+            ['<C-Up>'] = actions.cycle_history_prev,
+
             ['<C-j>'] = actions.move_selection_next,
             ['<C-k>'] = actions.move_selection_previous,
 
