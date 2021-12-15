e1b3e0af61271beafdc13214c801ad09277c884e
Josa Gesell <josa@gesell.me>
2021-12-15 23:33:09 +0100

    feat: Add tree sitter highlighting

--- lua/jg/layers/syntax--treesitter.lua
+++ lua/jg/layers/syntax--treesitter.lua
@@ -1,23 +1,14 @@
-if os.getenv('NVIM_TS') ~= 'true' then
-  return
-end
+local layer = require('jg.lib.layer')
 
-local plug = require('jg.lib.plug')
+layer.use({
+  require = { { 'nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' } } },
 
-plug.require({ 'nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' } })
-plug.after(function()
-  require('nvim-treesitter.configs').setup({
-    ensure_installed = 'maintained', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    -- playground = {
-    --     enable = true,
-    --     disable = {},
-    --     updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-    --     persist_queries = false -- Whether the query persists across vim sessions
-    -- },
-    highlight = { enable = true },
-    indent = { enable = true },
-    autotag = { enable = true },
-    rainbow = { enable = true },
-    context_commentstring = { enable = true, config = { javascriptreact = { style_element = '{/*%s*/}' } } },
-  })
-end)
+  after = function()
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = 'maintained', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+      highlight = { enable = true },
+      indent = { enable = false },
+      autotag = { enable = false },
+    })
+  end,
+})
