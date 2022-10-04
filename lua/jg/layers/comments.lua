acc3590d4a8d7d637cb89873d1fab3989261e9ca
Josa Gesell <josa@gesell.me>
2022-10-04 19:53:08 +0200

    feat: switch to Comment.nvim

--- lua/jg/layers/comments.lua
+++ lua/jg/layers/comments.lua
@@ -2,33 +2,19 @@ local layer = require('jg.lib.layer')
 
 layer.use({
   requires = {
-    'tomtom/tcomment_vim',
+    'numToStr/Comment.nvim',
   },
 
   map = {
-    { 'n', '#', '<Plug>TComment_gcc' },
-    { 'v', '#', '<Plug>TComment_gc' },
+    { 'n', '#', '<Plug>(comment_toggle_linewise_current)' },
   },
 
-  init = function()
-    vim.g.tcomment_maps = 1
-    vim.g.tcomment_mapleader1 = ''
-    vim.g.tcomment_mapleader2 = ''
-    vim.g.tcomment_opleader1 = 'gc'
-    vim.g.tcomment_mapleader_uncomment_anyway = ''
-    vim.g.tcomment_mapleader_comment_anyway = ''
-    vim.g.tcomment_textobject_inlinecomment = 'ic'
-  end,
-
   setup = function()
-    vim.fn['tcomment#type#Define']('jsonc', '// %s')
-    vim.fn['tcomment#type#Define']('jsonc_block', vim.g['tcomment#block_fmt_c'])
-    vim.fn['tcomment#type#Define']('jsonc_inline', vim.g['tcomment#inline_fmt_c'])
-    vim.fn['tcomment#type#Define']('json', '// %s')
-    vim.fn['tcomment#type#Define']('json_block', vim.g['tcomment#block_fmt_c'])
-    vim.fn['tcomment#type#Define']('json_inline', vim.g['tcomment#inline_fmt_c'])
-    vim.fn['tcomment#type#Define']('monkeyc', '// %s')
-    vim.fn['tcomment#type#Define']('monkeyc_block', vim.g['tcomment#block_fmt_c'])
-    vim.fn['tcomment#type#Define']('monkeyc_inline', vim.g['tcomment#inline_fmt_c'])
+    require('Comment').setup()
+
+    local ft = require('Comment.ft')
+    ft.monkeyc = { '//%s', '/*%s*/' }
+    ft.json = { '//%s', '/*%s*/' }
+    ft.jsonc = { '//%s', '/*%s*/' }
   end,
 })
