c234f6e85dbba2bb74014b2d7d0109c5d9e511c6
Josa Gesell <josa@gesell.me>
2022-11-15 08:31:12 +0100

    wip

--- lua/jg/layers/syntax.lua
+++ lua/jg/layers/syntax.lua
@@ -11,6 +11,8 @@ layer.use({
 
   requires = {
     { 'nvim-treesitter/nvim-treesitter', ['do'] = ':TSUpdate' },
+    { 'nvim-treesitter/nvim-treesitter-context' },
+    { 'nvim-treesitter/nvim-treesitter-textobjects' },
     { 'nvim-treesitter/playground' },
   },
 
@@ -23,15 +25,37 @@ layer.use({
   },
 
   setup = function()
-    local configs = require('nvim-treesitter.configs')
-
-    configs.setup({
+    require('nvim-treesitter.configs').setup({
       ensure_installed = ts_install,
       ignore_install = ts_ignore,
       highlight = { enable = true, disable = ts_disable },
       indent = { enable = true, disable = ts_disable },
       autotag = { enable = false },
+
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+          },
+        },
+      },
     })
+
+    require('treesitter-context').setup({
+      max_lines = 1,
+      mode = 'topline',
+      default = {
+        'class',
+        'function',
+        'method',
+      },
+    })
+
+    vim.cmd([[hi TreesitterContextBottom gui=underline guisp=#4B5263]])
+    vim.cmd([[hi link TreesitterContext Normal]])
   end,
 })
 
