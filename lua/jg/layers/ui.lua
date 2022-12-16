f1b0993d5c4841f4bd3f8aeb593b92c38777a513
Josa Gesell <josa@gesell.me>
2022-12-16 21:03:47 +0100

    feat: simple ui.input

--- lua/jg/layers/ui.lua
+++ lua/jg/layers/ui.lua
@@ -27,61 +27,6 @@ layer.use({
 
 layer.use({
   requires = {
-    'josa42/nvim-ui',
-  },
-})
-
-layer.use({
-  enabled = false,
-
-  requires = {
-    'stevearc/dressing.nvim',
-    'MunifTanjim/nui.nvim',
-  },
-
-  setup = function()
-    require('dressing').setup({
-      input = {
-        anchor = 'NW',
-        border = 'rounded',
-        default_prompt = '→',
-        override = function(conf)
-          conf.row = 1
-          return conf
-        end,
-      },
-      select = { enabled = false },
-    })
-  end,
-})
-
-layer.use({
-  enabled = false,
-
-  requires = { 'onsails/diaglist.nvim' },
-
-  map = function()
-    local diaglist = require('diaglist')
-    return {
-      -- { 'n', '<space>dw', diaglist.open_all_diagnostics },
-      { 'n', '<space>l', diaglist.open_buffer_diagnostics },
-    }
-  end,
-
-  setup = function()
-    require('diaglist').init({
-      -- optional settings
-      -- below are defaults
-      debug = false,
-
-      -- increase for noisy servers
-      debounce_ms = 150,
-    })
-  end,
-})
-
-layer.use({
-  requires = {
     'rcarriga/nvim-notify',
   },
   setup = function()
@@ -100,5 +45,11 @@ layer.use({
     })
 
     vim.notify = notify
+  end,
+})
+
+layer.use({
+  setup = function()
+    vim.ui.input = require('jg.lib.input')
   end,
 })
---
+++ lua/jg/lib/input.lua
@@ -0,0 +1,63 @@
+local function input_win(opts, on_confirm, win_opts)
+  win_opts = win_opts or {}
+  local prompt = opts.prompt or ''
+  local default = opts.default or ''
+
+  local ctx = {}
+  local bufnr = vim.api.nvim_create_buf(false, true)
+
+  vim.bo[bufnr].buftype = 'prompt'
+  vim.bo[bufnr].bufhidden = 'wipe'
+
+  vim.fn.prompt_setprompt(bufnr, '')
+
+  local function close(value)
+    pcall(vim.api.nvim_win_close, ctx.winid, true)
+    vim.defer_fn(function()
+      on_confirm(value)
+    end, 10)
+  end
+
+  local function confirm()
+    close(vim.api.nvim_buf_get_lines(0, 0, 1, true)[1])
+  end
+
+  vim.keymap.set({ 'i', 'n' }, '<CR>', confirm, {
+    silent = true,
+    buffer = bufnr,
+  })
+  vim.keymap.set({ 'i', 'n' }, '<ESC>', close, {
+    silent = true,
+    buffer = bufnr,
+  })
+
+  win_opts = vim.tbl_deep_extend(
+    'force',
+    {
+      border = 'single',
+      relative = 'cursor',
+      row = 1,
+      col = 0,
+      height = 1,
+      width = 32,
+      style = 'minimal',
+      focusable = true,
+    },
+    win_opts or {},
+    {
+      title = ' ' .. prompt .. ' ',
+    }
+  )
+
+  ctx.winid = vim.api.nvim_open_win(bufnr, true, win_opts)
+  vim.api.nvim_win_set_option(ctx.winid, 'winhighlight', 'Search:None')
+
+  vim.defer_fn(function()
+    vim.api.nvim_buf_set_text(bufnr, 0, 0, 0, 0, { default })
+    vim.cmd.startinsert({ bang = true })
+  end, 5)
+end
+
+return function(opts, on_confirm)
+  input_win(opts, on_confirm, {})
+end
