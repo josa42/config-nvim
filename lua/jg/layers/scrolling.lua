27682cbae92d7d33f4a8c2a4e2930d5da52797dd
Josa Gesell <josa@gesell.me>
2022-12-16 21:04:30 +0100

    feat: remove smooth scrolling from c-y and c-e

--- lua/jg/layers/scrolling.lua
+++ lua/jg/layers/scrolling.lua
@@ -3,6 +3,8 @@ local layer = require('jg.lib.layer')
 layer.use({
   requires = { 'karb94/neoscroll.nvim' },
   setup = function()
-    require('neoscroll').setup()
+    require('neoscroll').setup({
+      mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', 'zt', 'zz', 'zb' },
+    })
   end,
 })
