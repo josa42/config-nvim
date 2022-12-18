d3fbf0591f28ca8dcdeb6ebece910007797dca01
Josa Gesell <josa@gesell.me>
2022-12-18 22:21:19 +0100

    chore: refactor snippet snipet

--- snippets/json.lua
+++ snippets/json.lua
@@ -8,6 +8,7 @@ local c = luasnip.choice_node
 local sn = luasnip.snippet_node
 
 local rep = require('luasnip.extras').rep
+local fmt = require('luasnip.extras.fmt').fmt
 
 local paths = require('jg.lib.paths')
 local snipptes_dir = paths.config_dir .. '/snippets'
@@ -36,62 +37,58 @@ local function strip_prefix(tbl)
     end
   end
 
-  local l = {}
-  for _, str in ipairs(tbl) do
-    table.insert(l, str:sub(pre + 1))
+  for idx, str in ipairs(tbl) do
+    tbl[idx] = str:sub(pre + 1)
   end
 
-  return l
+  return tbl
 end
 
 return {
-  -- "${1}": {
-  --   "prefix": [
-  --     "${1}"
-  --   ],
-  --   "description": "${2}",
-  --   "body": [
-  --     "${3:$CLIPBOARD|empty}"
-  --   ],
-  -- }
-  s('snippet', {
-    t({ '"' }),
-    i(1),
-    t({ '": {', '\t"prefix": [', '\t\t"' }),
-    rep(1),
-    t({ '"', '\t],', '\t"description": "' }),
-    i(2),
-    t({ '",', '' }),
-    t({ '\t"body": [', '' }),
-    t('\t\t'),
+  s(
+    'snippet',
+    fmt(
+      [[
+        "{key}": {{
+        {__}"prefix": [
+        {__}{__}"{prefix}"
+        {__}],
+        {__}"description": "{description}",
+        {__}"body": [
+        {__}{__}{body}
+        {__}],
+        }}
+      ]],
+      {
+        key = rep(1),
+        prefix = i(1),
+        description = i(2),
+        body = c(3, {
+          f(function(args, snip)
+            if snip.env.CLIPBOARD == '$CLIPBOARD' then
+              return '$CLIPBOARD'
+            end
 
-    -- choice: clipboard or empty string
-    c(3, {
-      f(function(args, snip)
-        if snip.env.CLIPBOARD == '$CLIPBOARD' then
-          return '$CLIPBOARD'
-        end
+            local clipboard = vim.fn.getreg('+', 1, true)
 
-        local clipboard = vim.fn.getreg('+', 1, true)
-        local out = {}
+            if vim.tbl_islist(clipboard) then
+              for idx, line in ipairs(strip_prefix(clipboard)) do
+                local indent = idx == 1 and '' or '\t\t'
+                local comma = idx < #clipboard and ',' or ''
 
-        if vim.tbl_islist(clipboard) then
-          for idx, line in ipairs(strip_prefix(clipboard)) do
-            local indent = idx == 1 and '' or '\t\t'
-            local comma = idx < #clipboard and ',' or ''
+                clipboard[idx] = ('%s"%s"%s'):format(indent, line:gsub('"', '\\"'), comma)
+              end
+            end
 
-            table.insert(out, ('%s"%s"%s'):format(indent, line:gsub('"', '\\"'), comma))
-          end
-        end
-
-        return out
-      end),
-      sn(4, { t('"'), i(1), t('"'), i(2) }),
-    }),
-    t({ '', '' }),
-    t({ '\t],', '}' }),
-    i(0),
-  }, {
-    show_condition = is_json_snippet,
-  }),
+            return clipboard
+          end),
+          sn(4, { t('"'), i(1), t('"'), i(2) }),
+        }),
+        __ = t('\t'),
+      }
+    ),
+    {
+      show_condition = is_json_snippet,
+    }
+  ),
 }
