fce299609f894a69061c3b5d70c221f58bd664b2
Josa Gesell <josa@gesell.me>
2022-12-30 22:08:00 +0100

    feat: use relative line numbers

--- lua/jg/layers/builtin-options.lua
+++ lua/jg/layers/builtin-options.lua
@@ -8,6 +8,7 @@ local layer = require('jg.lib.layer')
 layer.use({
   init = function()
     vim.opt.number = true
+    vim.opt.relativenumber = true
     vim.opt.showmode = false
     vim.opt.cmdheight = 1
     vim.opt.signcolumn = 'yes'
