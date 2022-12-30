c251f6a237669d1fbc61288f84804ed7dc9612e6
Josa Gesell <josa@gesell.me>
2022-12-30 20:46:10 +0100

    feat: keymap to cycle through files in same directory

--- lua/jg/layers/files.lua
+++ lua/jg/layers/files.lua
@@ -1,5 +1,4 @@
 local layer = require('jg.lib.layer')
-
 layer.use({
   requires = {
     -- Files
@@ -20,5 +19,35 @@ layer.use({
         end
       end,
     })
+  end,
+})
+
+--------------------------------------------------------------------------------
+-- cycle through files in same directory
+
+layer.use({
+
+  map = function()
+    local function open_next(dir)
+      return function()
+        local dir_path = vim.fn.expand('%:p:h')
+        local file = vim.fn.expand('%:p')
+
+        local dir_files = vim.tbl_filter(function(dir_file)
+          return dir_file ~= '' and vim.fn.isdirectory(dir_file) == 0
+        end, vim.split(vim.fn.glob(('%s/*'):format(dir_path)), '\n'))
+
+        for idx, dir_file in ipairs(dir_files) do
+          if dir_file == file then
+            return vim.cmd.edit(dir_files[((idx - 1 + dir) % #dir_files) + 1])
+          end
+        end
+      end
+    end
+
+    return {
+      { 'n', '<leader>dj', open_next(1) },
+      { 'n', '<leader>dk', open_next(-1) },
+    }
   end,
 })
