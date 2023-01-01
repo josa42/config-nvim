962a2006e8a41b552ce178220f283a53350002b7
Josa Gesell <josa@gesell.me>
2023-01-02 00:55:55 +0100

    fix: clean up yamlls config

--- lua/jg/layers/lsp/yamlls.lua
+++ lua/jg/layers/lsp/yamlls.lua
@@ -2,29 +2,30 @@ local paths = require('jg.lib.paths')
 
 local settings = {
   yaml = {
-    disableAdditionalProperties = false,
-    schemaStore = { enable = true },
     schemas = {
-      ['file://' .. paths.config_home .. '/json-schema/tmuxinator.schema.json'] = {
-        '/Users/josa/.tmuxinator/*.yml',
-      },
-      ['file://' .. paths.config_home .. '/json-schema/bitbucket-pipeline.schema.json'] = {
-        'bitbucket-pipelines.yml',
-      },
-      ['file://' .. paths.config_home .. '/json-schema/alacritty.schema.json'] = {
-        'alacritty.yml',
-      },
-      ['http://json.schemastore.org/github-workflow'] = {
-        '.github/workflows/*.yml',
-      },
-      ['http://json.schemastore.org/github-action'] = {
-        '.github/actions/*/actions.yml',
-      },
+      -- ['file://' .. paths.config_home .. '/json-schema/tmuxinator.schema.json'] = {
+      --   '/Users/josa/.tmuxinator/*.yml',
+      -- },
+      -- ['file://' .. paths.config_home .. '/json-schema/bitbucket-pipeline.schema.json'] = {
+      --   'bitbucket-pipelines.yml',
+      -- },
+      -- ['file://' .. paths.config_home .. '/json-schema/alacritty.schema.json'] = {
+      --   'alacritty.yml',
+      -- },
+      -- ['http://json.schemastore.org/github-workflow'] = {
+      --   '.github/workflows/*.yml',
+      -- },
+      -- ['http://json.schemastore.org/github-action'] = {
+      --   '.github/actions/*/actions.yml',
+      -- },
       -- ['https://raw.githubusercontent.com/josa42/run/master/schema/tasks.json'] = {
       --   'tasks.yml'
       -- },
       ['file:///Users/josa/github/josa42/run/schema/tasks.json'] = {
         'tasks.yml',
+      },
+      ['file://' .. paths.home .. '/github/josa42/scheme-lazygit/lazygit.schema.json'] = {
+        paths.home .. '/.config/lazygit/config.yml',
       },
 
       -- ['https://goreleaser.com/schema.json'] = {
