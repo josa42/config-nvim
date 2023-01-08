fae01a75871128716be6d469f4a878e74f1c6234
Josa Gesell <josa@gesell.me>
2023-01-08 21:20:21 +0100

    chore: clean up yaml schemas

--- lua/jg/layers/lsp/yamlls.lua
+++ lua/jg/layers/lsp/yamlls.lua
@@ -3,24 +3,12 @@ local paths = require('jg.lib.paths')
 local settings = {
   yaml = {
     schemas = {
-      -- ['file://' .. paths.config_home .. '/json-schema/tmuxinator.schema.json'] = {
-      --   '/Users/josa/.tmuxinator/*.yml',
-      -- },
-      -- ['file://' .. paths.config_home .. '/json-schema/bitbucket-pipeline.schema.json'] = {
-      --   'bitbucket-pipelines.yml',
-      -- },
-      -- ['file://' .. paths.config_home .. '/json-schema/alacritty.schema.json'] = {
-      --   'alacritty.yml',
-      -- },
-      -- ['http://json.schemastore.org/github-workflow'] = {
-      --   '.github/workflows/*.yml',
-      -- },
-      -- ['http://json.schemastore.org/github-action'] = {
-      --   '.github/actions/*/actions.yml',
-      -- },
-      -- ['https://raw.githubusercontent.com/josa42/run/master/schema/tasks.json'] = {
-      --   'tasks.yml'
-      -- },
+      ['http://json.schemastore.org/github-workflow'] = {
+        '.github/workflows/*.yml',
+      },
+      ['http://json.schemastore.org/github-action'] = {
+        '.github/actions/*/actions.yml',
+      },
       ['file:///Users/josa/github/josa42/run/schema/tasks.json'] = {
         'tasks.yml',
       },
@@ -28,9 +16,9 @@ local settings = {
         paths.home .. '/.config/lazygit/config.yml',
       },
 
-      -- ['https://goreleaser.com/schema.json'] = {
-      --   '.goreleaser.yml'
-      -- }
+      ['https://goreleaser.com/schema.json'] = {
+        '.goreleaser.yml',
+      },
     },
   },
 }
