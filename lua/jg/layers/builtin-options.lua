5f78530ca954339dd13c29b8092d6bf9e49a3a36
Josa Gesell <josa@gesell.me>
2023-01-20 12:05:01 +0100

    feat: switch back to absolute line numbers

--- lua/jg/layers/builtin-options.lua
+++ lua/jg/layers/builtin-options.lua
@@ -8,7 +8,7 @@ local layer = require('jg.lib.layer')
 layer.use({
   init = function()
     vim.opt.number = true
-    vim.opt.relativenumber = true
+    vim.opt.relativenumber = false
     vim.opt.showmode = false
     vim.opt.cmdheight = 1
     vim.opt.signcolumn = 'yes'
