1936cbc27587ddcd3a3e4d6b4a1f178702eb6b44
Josa Gesell <josa@gesell.me>
2023-11-05 19:00:42 +0100

    feat: add more syntax plugins

---
+++ lua/plugins/syntax.lua
@@ -0,0 +1,14 @@
+return {
+  'zhaozg/vim-diagram',
+  'rhysd/vim-syntax-codeowners',
+  'darfink/vim-plist',
+  'josa42/vim-monkey-c',
+  'jxnblk/vim-mdx-js',
+  'mityu/vim-applescript',
+  'fladson/vim-kitty',
+  'antonk52/vim-browserslist',
+  'josa42/vim-trivyignore',
+  -- { 'josa42/vim-trivyignore', dir = '~/github/josa42/vim-trivyignore' },
+  'josa42/vim-npmrc',
+  -- { 'josa42/vim-npmrc', dir = '~/github/josa42/vim-npmrc' },
+}
