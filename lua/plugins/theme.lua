4144ef5361fcc793050868805f83a35ed2a44edf
Josa Gesell <josa@gesell.me>
2023-11-05 16:17:36 +0100

    feat: setup josa42/theme-theonedark

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'josa42/theme-theonedark',
+
+    name = 'theonedark',
+
+    config = function(plugin)
+      vim.opt.rtp:append(plugin.dir .. "/dist/nvim")
+
+      vim.o.termguicolors = true
+      vim.cmd.colorscheme('theonedark')
+    end,
+  }
+}
