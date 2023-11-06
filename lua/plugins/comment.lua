870b5ae9e8bdd277b43acba7b24e5ed867dc9cfb
Josa Gesell <josa@gesell.me>
2023-11-06 09:23:19 +0100

    feat: setup numToStr/Comment.nvim

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    event = { 'BufReadPost' },
+
+    'numToStr/Comment.nvim',
+    dependencies = {
+      'JoosepAlviste/nvim-ts-context-commentstring',
+    },
+
+    keys = {
+      { '#', '<Plug>(comment_toggle_linewise_current)' },
+      { '#', '<Plug>(comment_toggle_linewise_visual)', mode = { 'v' } },
+    },
+
+    config = function()
+      require('Comment').setup({
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      })
+
+      local ft = require('Comment.ft')
+      ft.monkeyc = { '//%s', '/*%s*/' }
+      ft.json = { '//%s', '/*%s*/' }
+      ft.jsonc = { '//%s', '/*%s*/' }
+      ft.gomod = { '//%s' }
+    end,
+  },
+}
