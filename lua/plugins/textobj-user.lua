547cc3807cf9954516ccca4b1e2e6ce2495e9e00
Josa Gesell <josa@gesell.me>
2023-12-05 21:34:26 +0100

    feat: setup kana/vim-textobj-user

---
+++ lua/plugins/textobj-user.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'kana/vim-textobj-user',
+    dependencies = {
+      'kana/vim-textobj-entire', --         ae | ie
+      'kana/vim-textobj-line', --           al | il
+      'kana/vim-textobj-indent', --         ai | ii
+      -- 'sgur/vim-textobj-parameter', --      a, | i,
+      'whatyouhide/vim-textobj-xmlattr', -- ax | ix
+      'fvictorio/vim-textobj-backticks', -- a` | i`
+    },
+  },
+}
