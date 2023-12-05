552476bf0c9d38ff6046ce35acd8f0cc977dfc61
Josa Gesell <josa@gesell.me>
2023-12-05 21:39:42 +0100

    feat: replace lukas-reineke/indent-blankline.nvim

--- lua/config/list.lua
+++ lua/config/list.lua
@@ -1,12 +1,27 @@
 -- White space
 vim.opt.list = true
+local lead_char = '┊'
 vim.opt.listchars = {
   -- eol = '↲',
-  tab = '» ',
+  -- eol = '↴',
+  tab = '┊ »',
   extends = '›',
   precedes = '‹',
-  nbsp = '·',
+  -- nbsp = '·',
   space = '·',
-  trail = '·',
-  -- eol = '↴',
+  -- trail = '·',
 }
+
+local function update_lead()
+  local lcs = vim.opt_local.listchars:get()
+  local lead = lead_char .. string.rep(lcs.space or ' ', vim.bo.shiftwidth - 1)
+  vim.opt_local.listchars:append({
+    -- leadmultispace = lead,
+    multispace = lead,
+  })
+end
+vim.api.nvim_create_autocmd(
+  'OptionSet',
+  { pattern = { 'listchars', 'shiftwidth', 'filetype' }, callback = update_lead }
+)
+vim.api.nvim_create_autocmd({ 'VimEnter', 'BufReadPost' }, { callback = update_lead, once = true })
