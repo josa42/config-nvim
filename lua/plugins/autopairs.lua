140241125d5d4c9f84df041a6dc0b9b84081ff33
Josa Gesell <josa@gesell.me>
2023-12-05 21:32:30 +0100

    feat: setup windwp/nvim-autopairs

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'windwp/nvim-autopairs',
+    opts = {
+      html_break_line_filetype = {
+        'html',
+        'javascript',
+        'javascriptreact',
+        'svelte',
+        'template',
+        'typescriptreact',
+        'vue',
+      },
+    },
+  },
+}
