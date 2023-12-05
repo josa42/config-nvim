db9c8dfbe4ef52800dbfbab2ff554955d792b24f
Josa Gesell <josa@gesell.me>
2023-12-05 21:33:56 +0100

    feat: setup kylechui/nvim-surround

---
+++ lua/plugins/surround.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    'kylechui/nvim-surround',
+    opts = {
+      keymaps = {
+        insert = nil, -- '<C-g>s',
+        insert_line = nil, -- '<C-g>S',
+        normal = 'sa',
+        normal_cur = nil, --  'yss',
+        normal_line = nil, --  'yS',
+        normal_cur_line = nil, --  'ySS',
+        visual = 'sa',
+        visual_line = nil, --  'gS',
+        delete = 'sd',
+        change = 'sr',
+      },
+      surrounds = {
+        v = { add = { '${', '}' } },
+      },
+    },
+  },
+}
