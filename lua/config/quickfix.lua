8137f068966da55e28c748df0014e7fd987c6045
Josa Gesell <josa@gesell.me>
2023-12-06 17:03:10 +0100

    feat: add dd keymap in quickfix

---
+++ lua/config/quickfix.lua
@@ -0,0 +1,19 @@
+local group = vim.api.nvim_create_augroup('qf.commands', { clear = true })
+
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  group = group,
+  pattern = { 'qf' },
+  callback = function(evt)
+    vim.keymap.set('n', 'dd', function()
+      local line = vim.fn.line('.')
+      vim.fn.setqflist(
+        vim.fn.filter(vim.fn.getqflist(), function(idx)
+          return idx ~= line - 1
+        end),
+        'r'
+      )
+      vim.api.nvim_win_set_cursor(0, { math.min(line, vim.fn.line('$')), 0 })
+    end, { buffer = evt.buf })
+  end,
+})
+
