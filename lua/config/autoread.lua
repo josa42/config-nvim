5c30ba73a2e4deb8f6ef1643eb67c1db4b07b24a
Josa Gesell <josa@gesell.me>
2023-12-10 22:40:07 +0100

    feat: create missing nested directories for new files

--- lua/config/autoread.lua
+++ lua/config/autoread.lua
@@ -18,3 +18,15 @@ vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter' }, {
   group = group,
   command = 'execute',
 })
+
+-- create missing nested directories for new files
+vim.api.nvim_create_autocmd('BufWritePre', {
+  callback = function()
+    local dir = vim.fn.expand('<afile>:p:h')
+    if type(dir) == 'string' then
+      if vim.fn.isdirectory(dir) == 0 then
+        vim.fn.mkdir(dir, 'p')
+      end
+    end
+  end,
+})
