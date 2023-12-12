c6380675f15d5a8fe9a6c95a36b0f5038241cc10
Josa Gesell <josa@gesell.me>
2023-12-12 10:54:02 +0100

    fix(conform): disable lsp fallback

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -22,7 +22,7 @@ return {
       format_on_save = {
         -- These options will be passed to conform.format()
         timeout_ms = 500,
-        lsp_fallback = true,
+        lsp_fallback = false,
       },
     },
     --
