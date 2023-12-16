8358e30e07c1d1d46e3a28cdfcce6583374a5c26
Josa Gesell <josa@gesell.me>
2023-12-16 12:40:15 +0100

    feat(telescope): add workflow searches

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,23 +4,23 @@ return {
     'nvim-telescope/telescope.nvim',
 
     dependencies = {
-      'josa42/nvim-telescope-config', --[[ dir = '~/github/josa42/nvim-telescope-config' ]]
+      {
+        'josa42/nvim-telescope-config', --[[ dir = '~/github/josa42/nvim-telescope-config' ]]
+      },
       'nvim-lua/plenary.nvim',
       'josa42/nvim-telescope-minimal-layout',
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
       'nvim-telescope/telescope-ui-select.nvim',
       'josa42/nvim-telescope-mask',
       'itchyny/vim-gitbranch',
-      'josa42/nvim-telescope-workspaces', --[[ dir = '~/github/josa42/nvim-telescope-workspaces' ]]
+      {
+        'josa42/nvim-telescope-workspaces', --[[ dir = '~/github/josa42/nvim-telescope-workspaces' ]]
+      },
     },
 
-    keys = function(plg)
+    keys = function()
       local builtin = require('telescope.builtin')
       local commands = require('telescope-config').commands
-      local global = commands.global
-      local root = commands.root
-      local workspace = commands.workspace
-      local config = commands.config
 
       return {
         { '<c-p>', commands.global.find_files, desc = 'Find files' },
@@ -32,30 +32,32 @@ return {
         -- p  -> find
         -- wp -> find in workspace
         -- rp -> find in repo root
-        { '<leader>p', global.find_files },
-        { '<leader>wp', workspace.find_files },
-        { '<leader>rp', root.find_files },
-        { '<leader>cp', config.find_files, desc = 'Find config' },
+        { '<leader>p', commands.global.find_files },
+        { '<leader>wp', commands.workspace.find_files },
+        { '<leader>rp', commands.root.find_files },
+        { '<leader>cp', commands.config.find_files, desc = 'Find config' },
+        { '<leader>ap', commands.workflows.find_files, desc = 'Find workflows' },
 
         -- Find String
         -- f  -> search
         -- wf -> search in workspace
         -- rf -> search in repo root
         -- cf -> search in config
-        { '<leader>f', global.find_string, desc = 'Find string' },
-        { '<leader>wf', workspace.find_string },
-        { '<leader>rf', root.find_string },
-        { '<leader>cf', config.find_string, desc = 'Find string in config' },
+        { '<leader>f', commands.global.find_string, desc = 'Find string' },
+        { '<leader>wf', commands.workspace.find_string },
+        { '<leader>rf', commands.root.find_string },
+        { '<leader>cf', commands.config.find_string, desc = 'Find string in config' },
+        { '<leader>af', commands.workflows.find_string, desc = 'Find string in workflows' },
 
         -- File Browser
         -- b  -> file browser
         -- wb -> file browser in workspace
         -- rb -> file browser in repo root
         -- cb -> file browser in config
-        -- { '<leader>b', global.file_browser },
-        -- { '<leader>wb', workspace.file_browser },
-        -- { '<leader>rb', root.file_browser },
-        -- { '<leader>cb', config.file_browser },
+        -- { '<leader>b', commands.global.file_browser },
+        -- { '<leader>wb', commands.workspace.file_browser },
+        -- { '<leader>rb', commands.root.file_browser },
+        -- { '<leader>cb', commands.config.file_browser },
 
         -- Git
         { '<leader>h', builtin.help_tags, desc = 'Find help' },
