a75349ec5f6a200aca156232c4b6cf0f81411f9f
Josa Gesell <josa@gesell.me>
2023-12-17 22:17:31 +0100

    feat(textobject-user): add sgur/vim-textobj-parameter

--- lua/plugins/textobj-user.lua
+++ lua/plugins/textobj-user.lua
@@ -5,9 +5,12 @@ return {
       'kana/vim-textobj-entire', --         ae | ie
       'kana/vim-textobj-line', --           al | il
       'kana/vim-textobj-indent', --         ai | ii
-      -- 'sgur/vim-textobj-parameter', --      a, | i,
+      'sgur/vim-textobj-parameter', --      aa | ia
       'whatyouhide/vim-textobj-xmlattr', -- ax | ix
       'fvictorio/vim-textobj-backticks', -- a` | i`
     },
+    init = function()
+      vim.g.vim_textobj_parameter_mapping = 'a'
+    end,
   },
 }
