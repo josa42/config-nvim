a27f5fc2f28d0f2ed7575e58a9127074759ccf2c
Josa Gesell <josa@gesell.me>
2024-01-03 11:01:52 +0100

    feat(typescript-tools): filter diagnostics

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -6,7 +6,18 @@ return {
       require('config.utils.mason').try_mason_install({
         'typescript-language-server',
       })
-      require('typescript-tools').setup({})
+
+      local api = require('typescript-tools.api')
+      require('typescript-tools').setup({
+        handlers = {
+          ['textDocument/publishDiagnostics'] = api.filter_diagnostics({
+            -- https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+            7016, --  Could not find a declaration file for module '<module>'.
+            80001, -- File is a CommonJS module; it may be converted to an ES6 module.
+            80002, -- This constructor function may be converted to a class declaration.
+          }),
+        },
+      })
     end,
   },
 }
