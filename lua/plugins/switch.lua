969f7c12b1d59cb6d71524719ea805bd87f55b49
Josa Gesell <josa@gesell.me>
2024-01-11 10:09:07 +0100

    feat(switch): add pattern to convert cjs imoirts to esm

--- lua/plugins/switch.lua
+++ lua/plugins/switch.lua
@@ -29,6 +29,8 @@ return {
               [ [[\v^( *)(it|describe|test)( *)\(]] ] = [[\1\2.only\3(]],
               [ [[\v^( *)(it|describe|test).only( *)\(]] ] = [[\1\2.skip\3(]],
               [ [[\v^( *)(it|describe|test).skip( *)\(]] ] = [[\1\2\3(]],
+              [ [[\v^(var|const) ([^{} ]+) \= require\(('[^']+')\)(;?)$]] ] = [[import \2 from \3\4]],
+              [ [[\v^(var|const) ([^{} ]+) \= require\(('[^']+')\)\.\2(;?)$]] ] = [[import { \2 } from \3\5]],
             },
           }
         end,
@@ -36,38 +38,3 @@ return {
     end,
   },
 }
--- layer.use({
---   requires = { 'AndrewRadev/switch.vim' },
---
---   autocmds = {
---     {
---       'FileType',
---       callback = function()
---         vim.b.switch_custom_definitions = {}
---       end,
---     },
---     {
---       'FileType',
---       pattern = [[\v(javascript|typescript)(react|)]],
---       callback = function()
---         vim.b.switch_custom_definitions = {
---           {
---             [ [[\v'([^'`]+)']] ] = [[`\1`]],
---             [ [[\v'([^'"]+)']] ] = [["\1"]],
---             [ [[\v`([^`"]+)`]] ] = [["\1"]],
---             [ [[\v`([^`']+)`]] ] = [['\1']],
---             [ [[\v"([^"']+)"]] ] = [['\1']],
---             [ [[\v"([^"`]+)"]] ] = [[`\1`]],
---             [ [[\v^( *)(it|describe|test)( *)\(]] ] = [[\1\2.only\3(]],
---             [ [[\v^( *)(it|describe|test).only( *)\(]] ] = [[\1\2.skip\3(]],
---             [ [[\v^( *)(it|describe|test).skip( *)\(]] ] = [[\1\2\3(]],
---           },
---         }
---       end,
---     },
---   },
---
---   init = function()
---     vim.g.switch_mapping = '-'
---   end,
--- })
