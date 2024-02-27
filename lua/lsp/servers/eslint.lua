7ae8b7f7298a33ceb8b5da3d5be51e3a06f02b31
Josa Gesell <josa@gesell.me>
2024-02-27 13:02:45 +0100

    wip(lsp): add todo to disable eslint fix if not available

--- lua/lsp/servers/eslint.lua
+++ lua/lsp/servers/eslint.lua
@@ -4,6 +4,24 @@ return function()
       quiet = true,
     },
 
+    -- TODO enable running fix only if eslint is available
+
+    -- on_init = function(client)
+    --   local path = client.workspace_folders[1].name
+    --
+    --   vim.notify('eslint: ' .. path, vim.log.levels.INFO)
+    --
+    --   -- if path == '/path/to/project1' then
+    --   --   client.config.settings['rust-analyzer'].checkOnSave.overrideCommand = { 'cargo', 'check' }
+    --   -- elseif path == '/path/to/rust' then
+    --   --   client.config.settings['rust-analyzer'].checkOnSave.overrideCommand =
+    --   --     { 'python3', 'x.py', 'check', '--stage', '1' }
+    --   -- end
+    --
+    --   -- client.notify('workspace/didChangeConfiguration', { settings = client.config.settings })
+    --   return true
+    -- end,
+
     on_attach = function(client, bufnr)
       vim.api.nvim_create_autocmd('BufWritePre', {
         buffer = bufnr,
